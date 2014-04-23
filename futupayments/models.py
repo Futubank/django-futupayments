49710e2cbd7681e719d0119de0b43f6599283a42
Maxim Oransky <maxim.oransky@gmail.com>
2014-04-23 21:45:59 +0400

    изменения в API

--- futupayments/models.py
+++ futupayments/models.py
@@ -6,21 +6,44 @@ class Payment(models.Model):
     STATE_COMPLETE = 'COMPLETE'
     STATE_FAILED = 'FAILED'
 
-    state_codes = (
+    STATE_CHOICES = (
         (STATE_COMPLETE, u'успешно'),
         (STATE_FAILED, u'ошибка'),
     )
 
-    creation_datetime = models.DateTimeField(u'время', auto_now_add=True)
-    transaction_id = models.BigIntegerField(u'ID транзакции в платежном шлюзе', db_index=True)
-    amount = models.DecimalField(u'сумма операции',
-                                 max_digits=10, decimal_places=2)
-    order_id = models.CharField(u'ID операции в магазине', max_length=128)
-    state = models.CharField(max_length=10, choices=state_codes)
-    response_message = models.TextField(
-        help_text=u'текст ошибки или сообщение об успешном совершении операции',
+    creation_datetime = models.DateTimeField(
+        u'время',
+        auto_now_add=True,
     )
-    meta = models.TextField(blank=True, default='')
+    transaction_id = models.BigIntegerField(
+        u'ID транзакции в платежном шлюзе',
+        db_index=True,
+    )
+    amount = models.DecimalField(
+        u'сумма операции',
+        max_digits=10,
+        decimal_places=2,
+    )
+    currency = models.CharField(
+        u'валюта',
+        max_length=3,
+    )
+    order_id = models.CharField(
+        u'ID операции в магазине',
+        max_length=128,
+    )
+    state = models.CharField(
+        u'состояние',
+        max_length=10,
+        choices=STATE_CHOICES,
+    )
+    message = models.TextField(
+        u'текст ошибки или сообщение об успешном совершении операции',
+        blank=True,
+    )
+    meta = models.TextField(
+        blank=True,
+    )
 
     def is_success(self):
         return self.state is self.STATE_COMPLETE
