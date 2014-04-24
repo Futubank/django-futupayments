947edc187aabb7c9ab4f1d274fb5e5bd48089b41
Maxim Oransky <maxim.oransky@gmail.com>
2014-04-24 20:13:39 +0400

    setup py fix

--- setup.py
+++ setup.py
@@ -4,5 +4,13 @@ from distutils.core import setup
 setup(
     name='django-futupayments',
     version='0.1',
-    packages=['futupayments'],
+    author='Maxim Oransky',
+    author_email='om@futubank.com',
+    packages=[
+        'futupayments'
+    ],
+    package_data={
+        'futupayments': ['templates/futupayments/*'],
+    },
+    url='https://github.com/Futubank/django-futupayments',
 )
