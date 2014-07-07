78fc0b542910d59e95b5f8eaa50db02edb817537
krukov <glebov.ru@gmail.com>
2014-07-07 17:23:36 +0400

    Revert "Using  `require_POST`"
    This reverts commit df47a60ab305bfe67e45f0d0a23e5c1e3c30ab54.

--- futupayments/views.py
+++ futupayments/views.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.views.decorators.http import require_POST
 
 from .forms import PaymentCallbackForm
 from .models import Payment
@@ -15,8 +14,9 @@ def fail(request):
     return render(request, 'futupayments/fail.html')
 
 
-@require_POST
 def callback(request):
+    if request.method.lower() != 'post':
+        raise Http404
     try:
         payment = Payment.objects.get(
             transaction_id=request.POST.get('transaction_id'),
