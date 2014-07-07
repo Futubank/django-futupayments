df47a60ab305bfe67e45f0d0a23e5c1e3c30ab54
krukov <glebov.ru@gmail.com>
2014-07-07 17:08:04 +0400

    Using  `require_POST`

---
+++ futupayments/tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase as DjangoTestCase
+
+from .forms import PaymentForm, PaymentCallbackForm
+
+class FutubankPaymentForm(DjangoTestCase):
+
+    def setUp(self):
+        pass
+
--- futupayments/views.py
+++ futupayments/views.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 from .forms import PaymentCallbackForm
 from .models import Payment
@@ -14,9 +15,8 @@ def fail(request):
     return render(request, 'futupayments/fail.html')
 
 
+@require_POST
 def callback(request):
-    if request.method.lower() != 'post':
-        raise Http404
     try:
         payment = Payment.objects.get(
             transaction_id=request.POST.get('transaction_id'),
