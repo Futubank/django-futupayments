aa0a1f6823167097ffac60555784f56ee0b77eda
krukov <dima@futubank.com>
2014-08-15 12:50:07 +0400

    Migration support python > 3

--- futupayments/migrations/0002_auto__add_field_payment_testing.py
+++ futupayments/migrations/0002_auto__add_field_payment_testing.py
@@ -9,23 +9,23 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding field 'Payment.testing'
-        db.add_column(u'futupayments_payment', 'testing',
+        db.add_column('futupayments_payment', 'testing',
                       self.gf('django.db.models.fields.BooleanField')(default=True),
                       keep_default=False)
 
 
     def backwards(self, orm):
         # Deleting field 'Payment.testing'
-        db.delete_column(u'futupayments_payment', 'testing')
+        db.delete_column('futupayments_payment', 'testing')
 
 
     models = {
-        u'futupayments.payment': {
+        'futupayments.payment': {
             'Meta': {'ordering': "('-creation_datetime',)", 'unique_together': "(('state', 'transaction_id'),)", 'object_name': 'Payment'},
             'amount': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2'}),
             'creation_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'currency': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'message': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'meta': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'order_id': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
