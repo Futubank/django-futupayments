80360d532a4b7019db1eab74c83435c4f0a0d6b1
krukov <dima@futubank.com>
2014-08-20 17:35:09 +0400

    Pipy release

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     name='django-futupayments',
     version=get_version(),
     author='Maxim Oransky',
+    license='MIT license',
     author_email='om@futubank.com',
     packages=[
         'futupayments',
@@ -21,4 +22,17 @@ setup(
     },
     url='https://github.com/Futubank/django-futupayments',
     requires=['django (>= 1.3)'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
